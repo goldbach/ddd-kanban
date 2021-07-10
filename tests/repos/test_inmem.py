d4204f3926438ab3bf47ef35f79ccdf7fc548fb5
Jakob Goldbach <jakob.goldbach@gmail.com>
2021-07-10 18:08:55 +0200

    json workitem test

--- tests/repos/test_inmem.py
+++ tests/repos/test_inmem.py
@@ -1,5 +1,6 @@
 from kanban.domain.model import WorkItem
 from kanban.infrastructure.repos.inmem_work_item_repo import InMemWorkItemRepo
+from kanban.infrastructure.repos.json_work_item_repo import JsonWorkItemRepo
 
 
 def test_inmem_putget_workitem():
@@ -9,6 +10,20 @@ def test_inmem_putget_workitem():
     }
     task = WorkItem.create(**data)
     repo = InMemWorkItemRepo()
+    repo.put(task)
+
+    retrieved_task = repo.work_item_by_id(task.id)
+    assert retrieved_task == task
+    assert retrieved_task is not task
+
+
+def test_json_putget_workitem(tmp_path):
+    data = {
+        'name': 'title',
+        'description': 'some text',
+    }
+    task = WorkItem.create(**data)
+    repo = JsonWorkItemRepo(tmp_path)
     repo.put(task)
 
     retrieved_task = repo.work_item_by_id(task.id)
