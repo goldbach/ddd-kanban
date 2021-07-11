38b750087fe75f5d3d6d601de82b8e487d99a2cb
Jakob Goldbach <jakob.goldbach@gmail.com>
2021-07-11 09:53:18 +0200

    workitem test all repos

--- tests/repos/test_inmem.py
+++ tests/repos/test_inmem.py
@@ -1,31 +1,34 @@
+import pytest
+
 from kanban.domain.model import WorkItem
 from kanban.infrastructure.repos.inmem_work_item_repo import InMemWorkItemRepo
 from kanban.infrastructure.repos.json_work_item_repo import JsonWorkItemRepo
 
-
-def test_inmem_putget_workitem():
-    data = {
-        'name': 'title',
-        'description': 'some text',
-    }
-    task = WorkItem.create(**data)
-    repo = InMemWorkItemRepo()
-    repo.put(task)
-
-    retrieved_task = repo.work_item_by_id(task.id)
-    assert retrieved_task == task
-    assert retrieved_task is not task
+workitem_data = {
+    'name': 'title',
+    'description': 'some text',
+}
 
 
-def test_json_putget_workitem(tmp_path):
-    data = {
-        'name': 'title',
-        'description': 'some text',
-    }
-    task = WorkItem.create(**data)
-    repo = JsonWorkItemRepo(tmp_path)
-    repo.put(task)
+@pytest.fixture()
+def inmem_workitem_repo():
+    return InMemWorkItemRepo()
 
-    retrieved_task = repo.work_item_by_id(task.id)
+
+@pytest.fixture()
+def json_workitem_repo(tmp_path):
+    return JsonWorkItemRepo(tmp_path)
+
+
+@pytest.fixture(params=['json_workitem_repo', 'inmem_workitem_repo'])
+def workitem_repo(request):
+    return request.getfixturevalue(request.param)
+
+
+def test_repo_putget_workitem(workitem_repo):
+    task = WorkItem.create(**workitem_data)
+    workitem_repo.put(task)
+
+    retrieved_task = workitem_repo.work_item_by_id(task.id)
     assert retrieved_task == task
     assert retrieved_task is not task
