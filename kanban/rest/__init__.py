7f814bd97f11444a720e510a80d4921031bcfdd0
Jakob Goldbach <jakob.goldbach@gmail.com>
2021-07-13 10:36:55 +0200

    new workitem rest api

--- kanban/rest/__init__.py
+++ kanban/rest/__init__.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 from kanban.domain.model.workitem import Repository as WorkItemRepo
 from kanban.domain.model.board import Repository as BoardRepo
-# from kanban.service.workitem import NewWorkItemCommand, NewWorkItemHandler
+from kanban.service.workitem import NewWorkItemCommand, NewWorkItemHandler
 from kanban.service.board import NewBoardCommand, NewBoardHandler
 
 from pydantic import BaseModel
@@ -10,6 +10,11 @@ from pydantic import BaseModel
 
 class NewBoard(BaseModel):
     name: str
+
+
+class NewWorkItem(BaseModel):
+    name: str
+    description: str
 
 
 def create_rest_app(board_repo: BoardRepo, work_item_repo: WorkItemRepo):
@@ -25,6 +30,11 @@ def create_rest_app(board_repo: BoardRepo, work_item_repo: WorkItemRepo):
         cmd = NewBoardCommand(board.name)
         handler = NewBoardHandler(board_repo)
         handler(cmd)
-        print(cmd)
+
+    @app.post('/workitem')
+    def post_new_work_item(workitem: NewWorkItem):
+        cmd = NewWorkItemCommand(name=workitem.name, description=workitem.description)
+        handler = NewWorkItemHandler(work_item_repo)
+        handler(cmd)
 
     return app
